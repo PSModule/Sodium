3da6ac76e79d33b0390adb4606fdc6c60681cb7e
Marius Storhaug <marstor@hotmail.com>
2024-04-12 19:41:53 +0200

    From Christian

--- examples/General.ps1
+++ examples/General.ps1
@@ -1,19 +1,57 @@
-﻿<#
-  .SYNOPSIS
-    This is a general example of how to use the module.
-#>
+﻿function New-GHRepositorySecret {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter()]
+        [String]
+        $Organization,
+        [Parameter()]
+        [String]
+        $RepositoryName,
+        [Parameter()]
+        [String]
+        $SecretName,
+        [Parameter()]
+        [String]
+        $SecretValue
+    )
+    try {
+        $ghToken = New-GHRepoToken
+        if (!$ghToken) {
+            throw 'No token retrieved'
+        }
+    } catch {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
 
-# Import the module
-Import-Module -Name 'PSModule'
+    $envpub = @{
+        Uri     = "https://api.github.com/repos/$Organization/$RepositoryName/actions/secrets/public-key"
+        Method  = 'Get'
+        Headers = @{
+            Accept                 = 'application/vnd.github+json'
+            Authorization          = "Bearer $($ghToken.token)"
+            'X-GitHub-Api-Version' = '2022-11-28'
+        }
+    }
 
-# Define the path to the font file
-$FontFilePath = 'C:\Fonts\CodeNewRoman\CodeNewRomanNerdFontPropo-Regular.tff'
+    $publicKey = Invoke-RestMethod @envpub
 
-# Install the font
-Install-Font -Path $FontFilePath -Verbose
-
-# List installed fonts
-Get-Font -Name 'CodeNewRomanNerdFontPropo-Regular'
-
-# Uninstall the font
-Get-Font -Name 'CodeNewRomanNerdFontPropo-Regular' | Uninstall-Font -Verbose
+    $envsecret = @{
+        Uri     = "https://api.github.com/repos/$Organization/$RepositoryName/actions/secrets/$SecretName"
+        Method  = 'PUT'
+        Headers = @{
+            Accept                 = 'application/vnd.github+json'
+            Authorization          = "Bearer $($ghToken.token)"
+            'X-GitHub-Api-Version' = '2022-11-28'
+        }
+        Body    = @{
+            encrypted_value = [Convert]::ToBase64String(
+                [Sodium.SealedPublicKeyBox]::Create(
+                    $SecretValue,
+                    [Convert]::FromBase64String($publicKey.Key)
+                )
+            )
+            key_id          = $publicKey.key_id
+        } | ConvertTo-Json
+    }
+    Invoke-RestMethod @envsecret
+}
