bb6712c4caad974e51c919b645e5742578ca2b70
Marius Storhaug <marstor@hotmail.com>
2024-10-07 11:40:56 +0200

    🚀 [Feature]: Restructure for Process-PSModule v3 (#7)
    ## Description
    
    - Removing support for Windows PowerShell - directional change for
    PSModule.
    - Remove nuget from Dependabot settings
    - Using Process-PSModule v3
    - Restructure for v3
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -8,16 +8,6 @@ param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'Sodium' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'Sodium' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'Sodium' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be importable' {
-            { Import-Module -Name 'Sodium' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
-
     Context "Function 'Test-PSModuleTemplate'" {
         It 'Should be able to call the function' {
             Test-PSModuleTemplate -Name 'World' | Should -Be 'Hello, World!'
