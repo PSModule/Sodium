06601162a3d738a855ab6d9677f3c40262c31992
Marius Storhaug <marstor@hotmail.com>
2024-11-02 18:08:34 +0100

    🩹 [Patch]: Remove redundant parameter from tests (#8)
    ## Description
    
    - Remove redundant parameter from tests
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'Sodium' {
+﻿Describe 'Sodium' {
     Context "Function 'Test-PSModuleTemplate'" {
         It 'Should be able to call the function' {
             Test-PSModuleTemplate -Name 'World' | Should -Be 'Hello, World!'
