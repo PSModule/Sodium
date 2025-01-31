a8dfd4dd8781f03ffa3743ed57eccd362ed58df0
Marius Storhaug <marstor@hotmail.com>
2025-01-31 10:00:23 +0100

    🩹 [Patch]: Uncomment OS architecture and description logging in main.ps1 for improved diagnostics

--- src/main.ps1
+++ src/main.ps1
@@ -1,25 +1,22 @@
-﻿# $arch = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToLower()
-# $os = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSDescription)
-# Write-Verbose "OS:              $os"
-# Write-Verbose "OS Architecture: $arch"
-# $libSodiumVersion = '1.0.19.2'
-# if ($IsWindows) {
-#     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\win-$arch\native\libsodium.dll"
-# }
-# if ($IsLinux) {
-#     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\linux-$arch\native\libsodium.so"
-# }
-# if ($IsMacOS) {
-#     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\osx-$arch\native\libsodium.dylib"
-# }
-# Write-Verbose "Library Path: $libPath"
+﻿$arch = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToLower()
+$os = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSDescription)
+Write-Verbose "OS:              $os"
+Write-Verbose "OS Architecture: $arch"
+$libSodiumVersion = '1.0.19.2'
+if ($IsWindows) {
+    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\win-$arch\native\libsodium.dll"
+}
+if ($IsLinux) {
+    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\linux-$arch\native\libsodium.so"
+}
+if ($IsMacOS) {
+    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\osx-$arch\native\libsodium.dylib"
+}
+Write-Verbose "Library Path:    $libPath"
 # $null = [System.Runtime.InteropServices.NativeLibrary]::Load($libPath)
+Add-Type -Path $libPath
 
-# $corePath = Join-Path $PSScriptRoot '\libs\Sodium.Core\1.3.5\Sodium.Core.dll'
+$corePath = Join-Path $PSScriptRoot '\libs\Sodium.Core\1.3.5\Sodium.Core.dll'
 # $null = [System.Reflection.Assembly]::LoadFile($corePath)
-# [Sodium.SodiumCore]::Init()
-
-
-$corePath = Join-Path $PSScriptRoot '\libs\Sodium.Core.dll'
 Add-Type -Path $corePath
 [Sodium.SodiumCore]::Init()
