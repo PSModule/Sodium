4455a27455b051c741c4a7a6d16d3d2de4b73346
Marius Storhaug <marstor@hotmail.com>
2025-01-31 04:12:14 +0100

    🩹 [Patch]: Update library paths for libsodium and Sodium.Core; remove obsolete manifest file

--- src/main.ps1
+++ src/main.ps1
@@ -1,15 +1,15 @@
 ﻿$arch = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToLower()
-
+4
 if ($IsWindows) {
-    $libPath = Join-Path $PSScriptRoot "\assemblies\libsodium\1.0.19.0\win-$arch\native\libsodium.dll"
+    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\1.0.19.0\win-$arch\native\libsodium.dll"
 }
 if ($IsLinux) {
-    $libPath = Join-Path $PSScriptRoot "\assemblies\libsodium\1.0.19.0\linux-$arch\native\libsodium.so"
+    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\1.0.19.0\linux-$arch\native\libsodium.so"
 }
 if ($IsMacOS) {
-    $libPath = Join-Path $PSScriptRoot "\assemblies\libsodium\1.0.19.0\osx-$arch\native\libsodium.dylib"
+    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\1.0.19.0\osx-$arch\native\libsodium.dylib"
 }
 $null = [System.Runtime.InteropServices.NativeLibrary]::Load($libPath)
 
-$corePath = Join-Path $PSScriptRoot '\assemblies\Sodium.Core\1.3.5\Sodium.Core.dll'
+$corePath = Join-Path $PSScriptRoot '\libs\Sodium.Core\1.3.5\Sodium.Core.dll'
 $null = [System.Reflection.Assembly]::LoadFile($corePath)
