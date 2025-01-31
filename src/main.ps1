3b26c85d4f02feb8217dec39cfe69c8d4eef3c2c
Marius Storhaug <marstor@hotmail.com>
2025-01-31 09:13:51 +0100

    🩹 [Patch]: Comment out legacy libsodium version loading code in main.ps1 for future removal

--- src/main.ps1
+++ src/main.ps1
@@ -1,17 +1,17 @@
-﻿$arch = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToLower()
-Write-Verbose "OS Architecture: $arch"
-$libSodiumVersion = '1.0.19.2'
-if ($IsWindows) {
-    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\win-$arch\native\libsodium.dll"
-}
-if ($IsLinux) {
-    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\linux-$arch\native\libsodium.so"
-}
-if ($IsMacOS) {
-    $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\osx-$arch\native\libsodium.dylib"
-}
-Write-Verbose "Library Path: $libPath"
-$null = [System.Runtime.InteropServices.NativeLibrary]::Load($libPath)
+﻿# $arch = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToLower()
+# Write-Verbose "OS Architecture: $arch"
+# $libSodiumVersion = '1.0.19.2'
+# if ($IsWindows) {
+#     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\win-$arch\native\libsodium.dll"
+# }
+# if ($IsLinux) {
+#     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\linux-$arch\native\libsodium.so"
+# }
+# if ($IsMacOS) {
+#     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\$libSodiumVersion\osx-$arch\native\libsodium.dylib"
+# }
+# Write-Verbose "Library Path: $libPath"
+# $null = [System.Runtime.InteropServices.NativeLibrary]::Load($libPath)
 
 $corePath = Join-Path $PSScriptRoot '\libs\Sodium.Core\1.3.5\Sodium.Core.dll'
 $null = [System.Reflection.Assembly]::LoadFile($corePath)
