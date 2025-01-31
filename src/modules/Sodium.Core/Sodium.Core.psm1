03f295d667542146cb274bbeae64a37bc80bc135
Marius Storhaug <marstor@hotmail.com>
2025-01-31 15:26:48 +0100

    Remove obsolete project files and clean up unused scripts

---
+++ src/modules/Sodium.Core/Sodium.Core.psm1
@@ -0,0 +1,21 @@
+﻿$arch = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToLower()
+$os = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSDescription)
+Write-Verbose "OS:               $os"
+Write-Verbose "OS Architecture:  $arch"
+if ($IsWindows) {
+    $libPath = Join-Path $PSScriptRoot "runtimes\win-$arch\native\libsodium.dll"
+}
+if ($IsLinux) {
+    $libPath = Join-Path $PSScriptRoot "runtimes\linux-$arch\native\libsodium.so"
+}
+if ($IsMacOS) {
+    $libPath = Join-Path $PSScriptRoot "runtimes\osx-$arch\native\libsodium.dylib"
+}
+Write-Verbose "libsodium Path:   $libPath"
+$null = [System.Runtime.InteropServices.NativeLibrary]::Load($libPath)
+
+$corePath = Join-Path $PSScriptRoot '\libs\Sodium.Core\1.3.5\Sodium.Core.dll'
+Write-Verbose "Sodium.Core Path: $corePath"
+$null = [System.Reflection.Assembly]::LoadFile($corePath)
+
+[Sodium.SodiumCore]::Init()
