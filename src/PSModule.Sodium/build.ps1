071d76063e999f3c567e1de44e66eaea2d66af31
Jason Vercellone <vercellone@users.noreply.github.com>
2025-01-31 04:12:23 -0600

    Initial codebase for PSModule.Sodium (#10)
    Sodium binary module with an IsolatedAssemblyLoadContext for Sodium.Core
    (libsodium).
    
    ## Description
    
    An implementation of [Loading through .NET Core Assembly Load
    Contexts](https://github.com/PSModule/Sodium/compare/main...vercellone:Sodium:feature/IsolatedAssemblyLoadContext?expand=1)
    to provide a ConvertTo-SodiumEncryptedString CmdLet as a foundation for
    GitHub secret encryption.
    
    - Inspired by [PSSodium](https://github.com/TylerLeonhardt/PSSodium) and
    [PowerShell-Yayaml](https://github.com/jborean93/PowerShell-Yayaml).
    - Based largely on
    [ModuleDependencyIsolationExample](https://github.com/rjmholt/ModuleDependencyIsolationExample).
    - Unlike PSSodium, this implementation targets pwsh 7.4+ / net8.0 (LTS)
    with no windows powershell.exe compatibility.
    - src/PSModule.Sodium/build.ps1 is excluding linux-musl-* runtime
    assemblies, but that could easily change.
    - src/PSModule.Sodium/build.ps1 publishes to ./lib/net8.0. The output
    path could easily be changed.
    - ./lib/** has been pushed to the repo. It could be excluded from the
    repo and built just-in-time by GitHub workflows and/or vscode task(s).
    But, IMHO including it simplifies local development scenarios.
    
    I suspect you'll want to PSModule-ize this a bit. But look it over and
    let me know if this is as useful a foundation as I think it is for
    GitHub secret encryption.
    
    ## Type of change
    
    - [x] 🚀 [Feature]
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    
    ---------
    
    Co-authored-by: Vercellone, Jason <Jason.Vercellone@mkcorp.com>

---
+++ src/PSModule.Sodium/build.ps1
@@ -0,0 +1,38 @@
+param(
+    [ValidateSet('Debug', 'Release')]
+    [string]
+    $Configuration = 'Release'
+)
+$repoRootPath = (Get-Item -Path $PSScriptRoot).Parent.Parent
+$outPath = Join-Path -Path $repoRootPath.FullName -ChildPath lib -AdditionalChildPath 'net8.0'
+if (Test-Path $outPath) {
+    Remove-Item -Path $outPath -Recurse
+}
+$isolatedLibPath = Join-Path -Path $outPath -ChildPath isolated
+$runtimesLibPath = Join-Path -Path $isolatedLibPath -ChildPath runtimes
+New-Item -Path $outPath -ItemType Directory
+New-Item -Path $isolatedLibPath -ItemType Directory
+New-Item -Path $runtimesLibPath -ItemType Directory
+
+Push-Location "$PSScriptRoot/PSModule.Sodium"
+try {
+    dotnet publish --configuration $Configuration
+}
+finally {
+    Pop-Location
+}
+
+# Copy selected unmanaged runtimes/[selected]/native/* assemblies to the isolated folder
+Get-ChildItem -Path "$PSScriptRoot/PSModule.Sodium/bin/$Configuration/net8.0/publish/runtimes" -Directory |
+    Where-Object { $_.Name -match "(win-x(64|86))|(linux-(arm|x)64)|(osx-(arm|x)64)" } |
+    ForEach-Object { Copy-Item -Path $_.FullName -Destination (Join-Path -Path $runtimesLibPath -ChildPath $_.Name) -Recurse }
+
+# Copy Nested Binary Module managed assemblies to lib
+Get-ChildItem -Path "$PSScriptRoot/PSModule.Sodium/bin/$Configuration/net8.0/publish" -File |
+    Where-Object { $_.BaseName -eq 'PSModule.Sodium' -and $_.Extension -in '.dll' } |
+    ForEach-Object { Copy-Item -LiteralPath $_.FullName -Destination $outPath }
+
+# Copy other managed assemblies to lib/isolated
+Get-ChildItem -Path "$PSScriptRoot/PSModule.Sodium/bin/$Configuration/net8.0/publish" -File |
+Where-Object { $_.BaseName -ne 'PSModule.Sodium' } |
+ForEach-Object { Copy-Item -LiteralPath $_.FullName -Destination $isolatedLibPath }
