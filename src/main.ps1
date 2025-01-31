be42d1c0706c453971bee581d22526d7303ca71a
Marius Storhaug <marstor@hotmail.com>
2025-01-31 04:22:45 +0100

    🩹 [Patch]: Add verbose logging for OS architecture and library path in main.ps1

--- src/main.ps1
+++ src/main.ps1
@@ -1,5 +1,5 @@
 ﻿$arch = ([string][System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToLower()
-4
+Write-Verbose "OS Architecture: $arch" -Verbose
 if ($IsWindows) {
     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\1.0.19.0\win-$arch\native\libsodium.dll"
 }
@@ -9,6 +9,7 @@ if ($IsLinux) {
 if ($IsMacOS) {
     $libPath = Join-Path $PSScriptRoot "\libs\libsodium\1.0.19.0\osx-$arch\native\libsodium.dylib"
 }
+Write-Verbose "Library Path: $libPath" -Verbose
 $null = [System.Runtime.InteropServices.NativeLibrary]::Load($libPath)
 
 $corePath = Join-Path $PSScriptRoot '\libs\Sodium.Core\1.3.5\Sodium.Core.dll'
