ebdfc1da3d0e5e9865edf43eacc1aa4b38b0156b
Marius Storhaug <marstor@hotmail.com>
2025-02-05 20:28:56 +0100

    🩹 [Patch]: Enhance ConvertTo-ByteArray function with detailed documentation and input validation

--- src/functions/private/ConvertTo-ByteArray.ps1
+++ src/functions/private/ConvertTo-ByteArray.ps1
@@ -1,6 +1,32 @@
 ﻿function ConvertTo-ByteArray {
+    <#
+        .SYNOPSIS
+        Converts a string into a byte array.
+
+        .DESCRIPTION
+        This function attempts to convert an input string into a byte array.
+        It first checks if the input is a Base64-encoded string and attempts to decode it.
+        If the Base64 decoding fails, it assumes the input consists of space-separated decimal values and converts them to bytes.
+
+        .EXAMPLE
+        ConvertTo-ByteArray "SGVsbG8gd29ybGQ="
+
+        Converts the Base64-encoded string "SGVsbG8gd29ybGQ=" into a byte array.
+
+        .EXAMPLE
+        ConvertTo-ByteArray "72 101 108 108 111"
+
+        Converts the space-separated decimal values into a byte array representing the string "Hello".
+
+        .NOTES
+        This function assumes that if the input is not valid Base64, it must be space-separated decimal values.
+    #>
+    [OutputType([byte[]])]
+    [CmdletBinding()]
     param (
-        [string]$InputString
+        # The input string to be converted into a byte array.
+        [Parameter(Mandatory = $true)]
+        [string] $InputString
     )
 
     # Check if it's a Base64 string first (GitHub API provides keys in Base64)
