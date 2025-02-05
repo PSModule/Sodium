7fb30b9a2ee3015f5b12038d1257c9ed96e70473
Marius Storhaug <marstor@hotmail.com>
2025-02-05 16:41:27 +0100

    🩹 [Cleanup]: Update module import paths to use 'libs' directory

--- src/main.ps1
+++ src/main.ps1
@@ -1,19 +1,19 @@
 switch ($true) {
     $IsLinux {
-        Import-Module "$PSScriptRoot/lib/linux-x64/PSModule.Sodium.dll"
+        Import-Module "$PSScriptRoot/libs/linux-x64/PSModule.Sodium.dll"
     }
     $IsMacOS {
         if ("$(sysctl -n machdep.cpu.brand_string)" -Like 'Apple*') {
-            Import-Module "$PSScriptRoot/lib/osx-arm64/PSModule.Sodium.dll"
+            Import-Module "$PSScriptRoot/libs/osx-arm64/PSModule.Sodium.dll"
         } else {
-            Import-Module "$PSScriptRoot/lib/osx-x64/PSModule.Sodium.dll"
+            Import-Module "$PSScriptRoot/libs/osx-x64/PSModule.Sodium.dll"
         }
     }
     $IsWindows {
         if ([System.Environment]::Is64BitProcess) {
-            Import-Module "$PSScriptRoot/lib/win-x64/PSModule.Sodium.dll"
+            Import-Module "$PSScriptRoot/libs/win-x64/PSModule.Sodium.dll"
         } else {
-            Import-Module "$PSScriptRoot/lib/win-x86/PSModule.Sodium.dll"
+            Import-Module "$PSScriptRoot/libs/win-x86/PSModule.Sodium.dll"
         }
     }
     default {
