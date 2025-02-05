70b2559a18d0e333084d1be21e451cacedb03738
Marius Storhaug <marstor@hotmail.com>
2025-02-05 19:39:10 +0100

    🩹 [Patch]: Update script execution method to use call operator for improved compatibility

--- src/main.ps1
+++ src/main.ps1
@@ -1,24 +1,23 @@
 switch ($true) {
     $IsLinux {
-        . "$PSScriptRoot/libs/linux-x64/main.ps1"
+        & "$PSScriptRoot/libs/linux-x64/main.ps1"
     }
     $IsMacOS {
         if ("$(sysctl -n machdep.cpu.brand_string)" -Like 'Apple*') {
-            . "$PSScriptRoot/libs/osx-arm64/main.ps1"
+            & "$PSScriptRoot/libs/osx-arm64/main.ps1"
         } else {
-            . "$PSScriptRoot/libs/osx-x64/main.ps1"
+            & "$PSScriptRoot/libs/osx-x64/main.ps1"
         }
-
     }
     $IsWindows {
         if ([System.Environment]::Is64BitProcess) {
-            . "$PSScriptRoot/libs/win-x64/main.ps1"
+            & "$PSScriptRoot/libs/win-x64/main.ps1"
         } else {
-            . "$PSScriptRoot/libs/win-x86/main.ps1"
+            & "$PSScriptRoot/libs/win-x86/main.ps1"
         }
     }
     default {
-        throw "Unsupported platform. Please refer to the documentation for more information."
+        throw 'Unsupported platform. Please refer to the documentation for more information.'
     }
 }
 
