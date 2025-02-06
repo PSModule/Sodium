a49896b08517322e7927dfabc973933f6911fd6d
Marius Storhaug <marstor@hotmail.com>
2025-02-06 14:40:54 +0100

    🩹 [Patch]: Downgrade project target framework to .NET 8.0 and update build script paths accordingly; remove obsolete clean script

--- PSModule/clean.ps1
+++
@@ -1,9 +0,0 @@
-$libPath = Join-Path -Path $PSScriptRoot -ChildPath lib
-Get-ChildItem -Path $libPath -Directory -Recurse | ForEach-Object {
-    Write-Warning "Deleting $($_.FullName)"
-    Remove-Item -Path $_.FullName -Recurse
-}
-Get-ChildItem -Path $PSScriptRoot -Directory -Recurse | Where-Object { $_.Name -in 'bin', 'obj' } | ForEach-Object {
-    Write-Warning "Deleting $($_.FullName)"
-    Remove-Item -Path $_.FullName -Recurse
-}
