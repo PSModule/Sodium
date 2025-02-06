a1a7acbe24f9032072d8d1a6e3cdce5cdf1ecfa2
Marius Storhaug <marstor@hotmail.com>
2025-02-06 17:19:08 +0100

    🚀 [Feature]: Update project to target .NET Standard 2.0 and adjust build configurations

--- PSModule/build.ps1
+++ PSModule/build.ps1
@@ -10,8 +10,8 @@ $targetRuntimes = @(
 
 Push-Location $PSScriptRoot
 $targetRuntimes | ForEach-Object {
-    dotnet publish -r $_
-    $source = "$PSScriptRoot/bin/Release/net8.0/$_/publish"
+    dotnet publish -r $_ --configuration Release
+    $source = "$PSScriptRoot/bin/Release/netstandard2.0/$_/publish"
     $destination = "$PSScriptRoot/../src/libs/$_"
     Copy-Item -Path $source -Destination $destination -Recurse -Force
 }
