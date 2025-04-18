967fcc68423cd604c2a9b13514de44b1a42f48ea
Marius Storhaug <marstor@hotmail.com>
2025-04-18 13:59:28 +0200

    🩹 [Patch]: Rename parameters in `crypto_box` functions for clarity and add `crypto_scalarmult_base` extern declaration

---
+++ src/functions/public/Get-SodiumPublicKey.ps1
@@ -0,0 +1,39 @@
+﻿function Get-SodiumPublicKey {
+    <#
+        .SYNOPSIS
+        Derives a Curve25519 public key from a provided private key using the Sodium cryptographic library.
+
+        .DESCRIPTION
+        Takes a Base64-encoded Curve25519 private key and returns the corresponding Base64-encoded public key. This is accomplished using the
+        Libsodium `crypto_scalarmult_base` function provided by the PSModule.Sodium .NET wrapper. The function ensures compatibility with
+        cryptographic operations requiring key exchange mechanisms.
+
+        .EXAMPLE
+        Get-SodiumPublicKey -PrivateKey 'ci5/7eZ0IbGXtqQMaNvxhJ2d9qwFxA8Kjx+vivSTXqU='
+
+        Output:
+        ```powershell
+        WQakMx2mIAQMwLqiZteHUTwmMP6mUdK2FL0WEybWgB8=
+        ```
+
+        Derives and returns the public key corresponding to the given Base64-encoded private key.
+
+        .OUTPUTS
+        string
+
+        .LINK
+        https://psmodule.io/Sodium/Functions/Get-SodiumPublicKey/
+    #>
+
+    [OutputType([string])]
+    [CmdletBinding()]
+    param(
+        # The private key to derive the public key from.
+        [Parameter(Mandatory)]
+        [string] $PrivateKey
+    )
+
+    ([Convert]::ToBase64String(
+        [PSModule.Sodium]::crypto_scalarmult_base(
+            [Convert]::FromBase64String($PrivateKey))))
+}
